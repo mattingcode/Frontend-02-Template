99190609a469d395eaed196f9629034d108aa0a4
mattingcode <ma_83814393@163.com>
2020-07-12 22:54:29 +0800

    update: 更新吧

--- week02/练习8.js
+++ week02/练习8.js
@@ -1,10 +1,25 @@
 function UTF8_Encoding(str) {
   const res = [];
+  let size = 0;
   for (let i =0; i< str.length; i++) {
-    const charCode = str.charCodeAt(i);
-    console.log(charCode);
-    res.push(charCode);
+    const s = str.charCodeAt(i);
+    if (s > 0x00 && s<= 0x7f) {
+      size += 1;
+      res.push(s);
+    } else if (0x80 <= s && s <= 0x7ff) {
+      size += 2;
+      res.push((192 | (31 & (s >> 6))));
+    } else if ((0x800 <= s && s <= 0xd7ff)  || (0xe000 <= code && code <= 0xffff)) {
+      size += 3;
+      res.push((224 | (15 & (s >> 12))));
+      res.push((128 | (63 & (s >> 6))));
+      res.push((128 | (63 & s)))
+    }
+  }
+  for (i = 0; i < res.length; i++) {
+    res[i] &= 0xff;
   }
   return res;
 }
 console.log(UTF8_Encoding('matt'));
+console.log(UTF8_Encoding('严'));
