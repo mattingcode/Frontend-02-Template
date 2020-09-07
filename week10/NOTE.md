200584bc1b79aca07e15f7669a8919f6f23ad415
马廷锋 <419194613@qq.com>
2020-09-07 19:52:56 +0800

    feat: add week10

--- week10/NOTE.md
+++ week10/NOTE.md
@@ -1,1 +1,4 @@
 学习笔记
+
+## 构建AST
+### LL（left left）
---
+++ week10/reg.js
@@ -0,0 +1,77 @@
+var regexp = /([0-9\.]+)|([ \t]+)|([\r\n]+)|(\*)|(\/)|(\+)|(\-)/g;
+var dictionary = ['Number', 'Whitespace', 'LineTerminator', '*', '/', '+', '-'];
+
+// function tokenize(source) {
+//   var result = null;
+//   while(true) {
+//     result = regexp.exec(source);
+//     if (!result) {
+//       break;
+//     }
+//     for (let i = 1; i<= dictionary.length; i++) {
+//       if (result[i]) {
+//         console.log(dictionary[i-1]);
+//       }
+//     }
+//     console.log(result);
+//   }
+// }
+function* tokenize(source) {
+  var result = null;
+  var lastIndex = 0;
+  while (true) {
+    lastIndex = regexp.lastIndex;
+    result = regexp.exec(source);
+    if (!result) {
+      break;
+    }
+    if (regexp.lastIndex - lastIndex > result[0].length) {
+      break;
+    }
+    let token = {
+      type: null,
+      value: null
+    };
+    for (let i = 1; i <= dictionary.length; i++) {
+      if (result[i]) {
+        token.type = dictionary[i-1];
+      }
+    }
+    token.value = result[0];
+    yield token;
+  }
+  yield {
+    type: 'EOF'
+  }
+}
+function Expression(tokens) {
+
+}
+function AdditiveExpression(source) {
+
+}
+function MultiplicativeExpression(source) {
+  if (source[0].type === 'Number') {
+    let node = {
+      type: 'MultiplicativeExpression',
+      children: [source[0]]
+    }
+    source[0] = node;
+    return MultiplicativeExpression(source);
+  }
+  if (source[0].type === 'MultiplicatvieExpression' && source[1] && source[1].type === '*') {
+    let node = {
+      type: 'MultiplicativeExpression',
+      operator: '*',
+      children: []
+    }
+    node.children.push(source.shift());
+    node.children.push(source.shift());
+    node.children.push(source.shift());
+    source.unshift(node);
+  }
+}
+for (let token of tokenize('1024 + 10 * 25')) {
+  console.log(token);
+}
+// tokenize('1024 + 10 * 25');
